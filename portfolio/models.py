d97e4e345c65a57762f1ef5f26e98e530856aa45
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-01 12:42:05 +0500

    Add Beckend Incom

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact_message(models.model):
+    name = models.CharField(max_length=50 , null=True , blank=True)
+    email = models.EmailField(max_length=100 , null=True , blank=True)
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def _str__(self):
+        return f"{self.name} and {self.email}" 
+
